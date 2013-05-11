ef1f58cd08f043b95bff46e5fc092a47f41a0b48
laetitia <laetitiavanespen@hotmail.com>
2013-05-11 16:34:49 +0200

    delete all php files

--- Project/include_scripts.php
+++
@@ -1,9 +0,0 @@
-
-    <script type="text/javascript" src="scripts/jquery-1.9.1.min.js" ></script>
-    <script type="text/javascript" src="scripts/jquery-ui-1.10.2.custom.js"></script>
-    <script type="text/javascript" src="scripts/bootstrap.min.js" ></script>
-    <script type="text/javascript" src="scripts/bootstrap-fileupload.min.js" ></script>
-    <script type="text/javascript" src="scripts/customScript.js" ></script>
-    <script type="text/javascript" src="scripts/jquery.backgroundpos.min.js" ></script>
-    <script type="text/javascript" src="scripts/humane.min.js" ></script>
-    
--- Project/include_styles.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap-responsive.min.css">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap-fileupload.min.css">
-<link rel="stylesheet" type="text/css" href="styles/customStyle.css">
-<link rel="stylesheet" type="text/css" href="styles/jackedup.css">
-<link rel="stylesheet"  type="text/css" href="styles/jquery-ui-1.10.2.custom.css" />
-
-    
