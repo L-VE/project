758b38db658c7c04f26efcdbbf82d5a8937094c7
laetitia <laetitiavanespen@hotmail.com>
2013-05-18 13:56:23 +0200

    added encrypted Project.rar

--- Project/include_notLoggedIn_error.php
+++
@@ -1,8 +0,0 @@
-<section class="Decorative" id="decorativeTop"></section>
-        		<div id='notloggedin' class='alert alert-error'>
-                <i class="icon-warning-sign"></i>
-                	Je bent niet ingelogd en kan deze pagina dus niet bekijken.
-                    Ga terug naar de  <strong><a href="login.php">login-pagina</a></strong> om in te loggen!
-                </div>
-<section class="Decorative" id="decorativeBottom"></section>
-  
